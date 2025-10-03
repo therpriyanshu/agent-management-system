68f371a9f8afb2c8e191ee2cfea038212c65ee6e
therpriyanshu <priyanshukr102004@gmail.com>
2025-10-03 23:54:13 +0530

    base change in vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://localhost:5000", // your local backend
         changeOrigin: true,
       },
     },
   },
+  base: "/", // needed for Netlify
 });
