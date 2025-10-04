02ad334135e9bc8d1332fb445e7958d02683bb11
therpriyanshu <priyanshukr102004@gmail.com>
2025-10-04 16:50:29 +0530

    change .env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:5000", // your local backend
+        target: "http://localhost:5000",
         changeOrigin: true,
       },
     },
   },
-  base: "/", // needed for Netlify
 });
